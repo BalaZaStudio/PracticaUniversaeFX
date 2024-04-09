afc43a1ee468ac6970ac282d3ce304671c26a2b0
ZaBaDeV <zabal_9@hotmail.com>
2024-04-09 13:04:56 +0200

    Proyecto Maven

---
+++ ProyectoPracticas/src/main/java/com/myproyectodam/proyectopracticas/ProyectoPracticas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myproyectodam.proyectopracticas;
+
+/**
+ *
+ * @author zabal
+ */
+public class ProyectoPracticas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
